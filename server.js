11606068d7da01d540b155bdfea075c9f5fc5fe4
Chelsie L Bangloy-Gonsalves <clbgonsalves@gmail.com>
2022-01-19 15:30:05 -1000

    wrap up commit

--- server.js
+++ server.js
@@ -1,12 +1,18 @@
 const express = require('express');
+
+const PORT = process.env.PORT || 3001;
 const app = express();
+const apiRoutes = require('./routes/apiRoutes');
+const htmlRoutes = require('./routes/htmlRoutes');
 
-app.get('/api/animals', (req, res) => {
-    res.json(animals);
-  });
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static('public'));
 
-app.listen(3001, () => {
-    console.log(`API server now on port 3001!`);
-  });
+// Use apiRoutes
+app.use('/api', apiRoutes);
+app.use('/', htmlRoutes);
 
-  const { animals } = require('./data/animals');
+app.listen(PORT, () => {
+  console.log(`API server now on port ${PORT}!`);
+});
